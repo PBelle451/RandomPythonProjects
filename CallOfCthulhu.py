7d5ff40081cf2bd8a94b96402789d1adbeb63d3b
Pedro Belle <99338512+PBelle451@users.noreply.github.com>
2023-03-08 21:03:54 -0300

    Add files via upload

--- CallOfCthulhu.py
+++ CallOfCthulhu.py
@@ -36,6 +36,48 @@ def pissChamber():
         else:
             print("Please insert a valid option")
         
+def altarChamber():
+    directions = ["forward", "backward"]
+    global torch
+    global necronomicon
+    print("You enter an altar chamber, dimly lit torches fashion the walls but you can still notice a stone statue at the very end of the hall")
+    userInput = ""
+    while userInput not in directions:
+        print("Options: forward/backward")
+        userInput = input()
+        if userInput == "forward":
+            if torch == True:
+                print("You walk towards the stone statue, you brandish your torch over it and notice it's an altar for the deity Cthulhu. Looking at the feet of the statue, you find a book bound in leather, you pick it up")
+                necronomicon = True
+            else:
+                print("You walk towards to the statue but you can't see anything.")
+        elif userInput == "backward":
+            introScene()
+        elif userInput == "left":
+            print("You bump into a wall")
+        elif userInput == "right":
+            print("YOu bump into a wall")
+        else:
+            print("Please insert a valid option")
+
+def diningHall():
+    directions = ["left", "right", "backward"]
+    print("You enter a room with a large wooden table at the center, you feel a putrid scent upon entering and look at several plates with food that looks inedible.")
+    userInput = ""
+    while userInput not in directions:
+        print("Options: left/right/backward")
+        userInput = input()
+        if userInput == "left":
+            oldGear()
+        elif userInput == "right":
+            Pantry()
+        elif userInput == "backward":
+            introScene()
+        elif userInput == "forward":
+            print("You bump into a wall")
+        else:
+            print("Please insert a valid option")
+
 
 if __name__ == '__main__':
     print("Welcome to Call of Cthulhu.")
